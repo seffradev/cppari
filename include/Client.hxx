b55a7f52696e82c7bd220873d7f2bd1f53d3f460
Hampus Avekvist <ha@leissner.se>
2024-09-18 20:20:14 +0200

    feat(client): start implementing `Client` methods

--- include/Client.hxx
+++ include/Client.hxx
@@ -1,11 +1,30 @@
 #pragma once
 
+#include <string>
+
 namespace cppari {
+
+using namespace std;
+
+#define ARI_URL_ENDPOINT "/ari"
 
 class Client {
 public:
+  Client(string url, string application, string username, string password);
+
 protected:
 private:
+  const string &getUrl() const;
+  const string &getApplication() const;
+  const string &getUsername() const;
+  const string &getPassword() const;
+  const string getAuthenticationString() const;
+  const string getWebsocketUrl() const;
+
+  string url;
+  string application;
+  string username;
+  string password;
 };
 
 } // namespace cppari
--- src/Client.cxx
+++ src/Client.cxx
@@ -1,3 +1,21 @@
 #include <Client.hxx>
 
 using namespace cppari;
+
+Client::Client(string url, string application, string username, string password)
+    : url(url), application(application), username(username),
+      password(password) {}
+
+const string &Client::getUrl() const { return url; }
+
+const string &Client::getApplication() const { return application; }
+
+const string &Client::getUsername() const { return username; }
+
+const string &Client::getPassword() const { return password; }
+
+const string Client::getAuthenticationString() const {
+  return getUsername() + ":" + getPassword();
+}
+
+const string Client::getWebsocketUrl() const { return url + "/ari"; }
